9473778cb851d4d8dabe15956de42da443d46635
Fer Developer <milton.vasquez0726@gmail.com>
2022-01-11 11:16:06 -0500

    Privacy updated

--- CocornaStore/Startup.cs
+++ CocornaStore/Startup.cs
@@ -63,7 +63,7 @@ namespace CocornaStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Products}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
