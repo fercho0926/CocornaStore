b631e051bd8d9f36786f05c27770f73ae9ea62a5
Fer Developer <milton.vasquez0726@gmail.com>
2022-01-11 10:50:36 -0500

    Initial Creation

---
+++ CocornaStore/Models/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CocornaStore.Models
+{
+    public class Products
+
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(80)]
+        public string Name { get; set; }
+
+        [Required, StringLength(80)]
+        public string Brand { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public DateTime DateUpdate { get; set; }
+    }
+
+}
